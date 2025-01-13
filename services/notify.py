f2e1c3049732c466c8159bbd41d9e43869eaa91c
JG_Erbes <jg.dev.new@gmail.com>
2025-01-13 13:21:20 -0300

    implementação webhook

--- services/notify.py
+++ services/notify.py
@@ -4,10 +4,10 @@ import requests
 class Notify:
 
     def __init__(self):
-        self.__base_url = 'https://webhook.site'
+        self.__base_url = 'http://localhost:8001'
 
-    def send_event(self, data):
+    def send_order_event(self, data):
         requests.post(
-            url=f'{self.__base_url}/3b1744c5-b432-4183-8e91-2b619d071a49',
+            url=f'{self.__base_url}/api/v1/webhooks/order/',
             json=data,
         )
