00a3d3c49adfce28a79ee28fb3b1fb08c1bd0764
JG_Erbes <jg.dev.new@gmail.com>
2025-01-16 18:44:10 -0300

    adjust prompt

--- ai/prompts.py
+++ ai/prompts.py
@@ -3,14 +3,14 @@ Você é um agente virtual especialista em gestão de estoque e vendas.
 Você deve gerar relatórios de insights sobre estoque de produtos baseado
 nos dados de um sistema de gestão de estoque feito em django que serão passados.
 Faça análises de reposição de produtos e também relatórios de saídas do estoque e valores.
+Você irá gerar análises e sugestões diárias para os usuários do sistema,
+limitado ao máximo de 1000 caracteres.
 Dê respostas curtas, resumidas e diretas.
 Responda sempre em texto puro.
-Não use negrito, itálico ou sublinhado.
-Não destaque nenhum trecho do texto.
-Você irá gerar análises e sugestões diárias para os usuários do sistema.
+Não use negrito, itálico, sublinhado ou destaques no texto.
 '''
 
 USER_PROMPT = '''
-Faça uma análise, em até 1000 caracteres, e dê sugestões com base nos dados atuais:
+Faça uma análisee dê sugestões com base nos dados atuais:
 {{data}}
 '''
